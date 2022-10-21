cf37b978430b15655b6a36fca5a7cf3dbfab10f3
Arshad <arshad.syed.anwar@gmail.com>
2022-10-21 01:04:44 +0100

    doctors route ready

--- routes/doctors.js
+++ routes/doctors.js
@@ -16,52 +16,67 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
   console.log(req.url, "@", Date.now());
   next();
 });
 
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
     router
       .route("/")
-      .get(async(req, res) => {
+      .get(async (req, res) => {
         ///doctors
-        const doctorsCollection = client.db(process.env.DB).collection('doctors');
+        const doctorsCollection = client
+          .db(process.env.DB)
+          .collection("doctors");
         const query = {};
         const cursor = doctorsCollection.find(query);
         const doctors = await cursor.toArray();
         res.send(doctors);
       })
-      .post(async(req, res) => {
+      .post(async (req, res) => {
         const newDoctor = req.body;
         console.log(newDoctor);
-        const doctorsCollection = client.db(process.env.DB).collection('doctors');
+        const doctorsCollection = client
+          .db(process.env.DB)
+          .collection("doctors");
         const result = await doctorsCollection.insertOne(newDoctor);
         res.send(result);
       });
 
+    router.route("/specialities").get(async (req, res) => {
+      ///doctors
+      const doctorsCollection = client.db(process.env.DB).collection("doctors");
+      const specialities = await doctorsCollection.distinct("speciality");
+      res.send(specialities);
+    });
+
     router
       .route("/:id")
-      .get(async(req, res) => {
+      .get(async (req, res) => {
         const id = req.params.id;
-        const doctorsCollection = client.db(process.env.DB).collection('doctors');
+        const doctorsCollection = client
+          .db(process.env.DB)
+          .collection("doctors");
         const query = {};
         const cursor = doctorsCollection.find(query);
         let doctor = await cursor.toArray();
         doctor = await doctor.filter((doctor) => doctor._id == id);
         res.send(doctor);
       })
-      .post(async(req, res) => {
+      .post(async (req, res) => {
         const id = req.params.id;
         console.log(id);
         console.log(req.body);
         const query = { _id: ObjectId(id) };
-        const doctorsCollection = client.db(process.env.DB).collection('doctors');
+        const doctorsCollection = client
+          .db(process.env.DB)
+          .collection("doctors");
         let doctor = await doctorsCollection.findOne(query);
-        console.log(doctor);      
-        doctor = {...doctor, ...req.body};
+        console.log(doctor);
+        doctor = { ...doctor, ...req.body };
         const result = await doctorsCollection.updateOne(
           { _id: ObjectId(id) },
           { $set: doctor }
@@ -69,8 +84,7 @@ async function run(){
         const newResult = await doctorsCollection.findOne(query);
         res.send(newResult);
       });
-  }finally{
-
+  } finally {
   }
 }
 
