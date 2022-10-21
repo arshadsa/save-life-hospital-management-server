e26bb14ea00916562d63bc711d239dea54dc1f81
Emranul_farhad <emranulfarhad@gmail.com>
2022-10-21 16:16:09 +0600

    local port changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const doctors = require("./routes/doctors");
 const pharmacy = require("./routes/pharmacy");
 const lab = require("./routes/lab");
