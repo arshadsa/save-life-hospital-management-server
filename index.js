7f39dd79ca48997df804b9fc162a18db4684224c
Ovilash Moitra <ovilashmoitraofficial@gmai.com>
2022-11-01 17:53:34 +0600

    port add

--- index.js
+++ index.js
@@ -508,4 +508,4 @@ app.listen(port, () => {
   console.log("Listening to port", port);
 });
 
-server.listen(process.env.PORT || 8000, () => console.log("server is running on port 8000"))
+server.listen(process.env.PORT, () => console.log("server is running on port 8000"))
